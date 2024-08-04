a032d02d6a7f3e7c755f5b6ea1ec7301747f94cb
tubarao19 <miguelmonsalvo19@gmail.com>
2024-08-03 22:44:40 -0500

    reto2_complete

--- retos/test.py
+++ retos/test.py
@@ -1,10 +1,6 @@
-# oracion = input("Digite una oracion\n>>> ")
-# lista_palabras = oracion.split()
+# Crea una lista
+lista = list(range(1,11))
+lista2 = [5, 6, 7, 8, 9, 10, 1, 2, 3, 4]
+lista3 = [23, 87, 45, 12, 67, 34, 78, 90, 56, 11]
 
 
-
-# print(lista_palabras)
-numeros = list(range(1,11))
-print(numeros)
-resultado = list(map(lambda num:num**2 ,numeros))
-print(resultado)
